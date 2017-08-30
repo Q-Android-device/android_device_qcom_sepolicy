d0f2c20d2e0392e2d29bdcf5f39aa80db08eaf7a
Rashed Abdel-Tawab <rashed@linux.com>
2017-08-29 20:58:26 -0700

    sepolicy: Move definitions from Android.mk to make opt-in
    Move the BOARD definitions to sepolicy.mk to allow devices to opt
    in rather then automatically include with Android.mk. Also include
    lineage-specific qcom sepolicy now located in
    device/lineage/sepolicy/qcom.
    
    Original commits:
    
    https://github.com/LineageOS/android_device_qcom_sepolicy/commit/959d3362451fe081d9a25fde87e989ffe90ab159
    
    https://github.com/LineageOS/android_device_qcom_sepolicy/commit/27f078f1041497ecadf1315b39f130882177ea64
    
    Change-Id: Ibf70e4c8ab9d91b50c62c3e9f1263e1624e8ca00
    
    # Conflicts:
    #	sepolicy.mk

--- sepolicy.mk
+++ sepolicy.mk
@@ -1,51 +1,30 @@
 # Board specific SELinux policy variable definitions
-BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
-    device/qcom/sepolicy/generic/public
+BOARD_SEPOLICY_DIRS += \
+    device/qcom/sepolicy/vendor/common \
+    device/qcom/sepolicy/vendor/ssg \
+    device/qcom/sepolicy/vendor/common/sysmonapp
 
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/qcom/sepolicy/generic/private
-
-BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
-    device/qcom/sepolicy/qva/public
-
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/qcom/sepolicy/qva/private
-
-ifneq (,$(filter $(UM_4_14_FAMILY), $(TARGET_BOARD_PLATFORM)))
-    BOARD_SEPOLICY_DIRS += \
-        device/qcom/sepolicy/generic/vendor/common \
-        device/qcom/sepolicy/qva/vendor/common/sysmonapp \
-        device/qcom/sepolicy/qva/vendor/ssg \
-        device/qcom/sepolicy/generic/vendor/timeservice \
-        device/qcom/sepolicy/qva/vendor/common
-
-    ifeq ($(TARGET_SEPOLICY_DIR),)
-      BOARD_SEPOLICY_DIRS += device/qcom/sepolicy/generic/vendor/$(TARGET_BOARD_PLATFORM)
-      BOARD_SEPOLICY_DIRS += device/qcom/sepolicy/qva/vendor/$(TARGET_BOARD_PLATFORM)
-    else
-      BOARD_SEPOLICY_DIRS += device/qcom/sepolicy/generic/vendor/$(TARGET_SEPOLICY_DIR)
-      BOARD_SEPOLICY_DIRS += device/qcom/sepolicy/qva/vendor/$(TARGET_SEPOLICY_DIR)
-    endif
-
-    ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
-      BOARD_SEPOLICY_DIRS += device/qcom/sepolicy/generic/vendor/test
-      BOARD_SEPOLICY_DIRS += device/qcom/sepolicy/qva/vendor/test
-    endif
+ifeq ($(TARGET_SEPOLICY_DIR),)
+BOARD_SEPOLICY_DIRS += device/qcom/sepolicy/vendor/$(TARGET_BOARD_PLATFORM)
 else
-    BOARD_SEPOLICY_DIRS += \
-        device/qcom/sepolicy/legacy/vendor/common/sysmonapp \
-        device/qcom/sepolicy/legacy/vendor/ssg \
-        device/qcom/sepolicy/legacy/vendor/timeservice \
-        device/qcom/sepolicy/legacy/vendor/common
-
-    ifeq ($(TARGET_SEPOLICY_DIR),)
-      BOARD_SEPOLICY_DIRS += device/qcom/sepolicy/legacy/vendor/$(TARGET_BOARD_PLATFORM)
-    else
-      BOARD_SEPOLICY_DIRS += device/qcom/sepolicy/legacy/vendor/$(TARGET_SEPOLICY_DIR)
-    endif
-    ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
-      BOARD_SEPOLICY_DIRS += device/qcom/sepolicy/legacy/vendor/test
-    endif
+BOARD_SEPOLICY_DIRS += device/qcom/sepolicy/vendor/$(TARGET_SEPOLICY_DIR)
 endif
+
+ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
+BOARD_SEPOLICY_DIRS += \
+    device/qcom/sepolicy/vendor/test
+endif
+
+ifneq (,$(filter 24 25 26 27, $(PRODUCT_SHIPPING_API_LEVEL)))
+BOARD_SEPOLICY_DIRS += \
+    device/qcom/sepolicy/vendor/ota/$(TARGET_BOARD_PLATFORM)
+endif
+
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
+    device/qcom/sepolicy/public
+
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    device/qcom/sepolicy/private
+>>>>>>> 4ace41a1... sepolicy: Move definitions from Android.mk to make opt-in
 
 -include device/lineage/sepolicy/qcom/sepolicy.mk
