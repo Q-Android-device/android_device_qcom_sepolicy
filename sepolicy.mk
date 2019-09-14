bbf47270eae695987594913a9e4bfc4d43979e8e
Volodymyr Zhdanov <wight554@gmail.com>
2019-09-14 12:48:09 +0300

    sepolicy: Fix target selection
    Anything lower or equals to sdm845 is now considered legacy.
    
    Change-Id: Id4e2433304396c8ca7430d3308f65d8d878f9bb9
    
    # Conflicts:
    #	sepolicy.mk

--- sepolicy.mk
+++ sepolicy.mk
@@ -1,29 +1,51 @@
 # Board specific SELinux policy variable definitions
-BOARD_SEPOLICY_DIRS += \
-    device/qcom/sepolicy-legacy-um/vendor/common \
-    device/qcom/sepolicy-legacy-um/vendor/ssg \
-    device/qcom/sepolicy-legacy-um/vendor/common/sysmonapp
-
-ifeq ($(TARGET_SEPOLICY_DIR),)
-BOARD_SEPOLICY_DIRS += device/qcom/sepolicy-legacy-um/vendor/$(TARGET_BOARD_PLATFORM)
-else
-BOARD_SEPOLICY_DIRS += device/qcom/sepolicy-legacy-um/vendor/$(TARGET_SEPOLICY_DIR)
-endif
-
-ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
-BOARD_SEPOLICY_DIRS += \
-    device/qcom/sepolicy-legacy-um/vendor/test
-endif
-
-ifneq (,$(filter 24 25 26 27, $(PRODUCT_SHIPPING_API_LEVEL)))
-BOARD_SEPOLICY_DIRS += \
-    device/qcom/sepolicy-legacy-um/vendor/ota/$(TARGET_BOARD_PLATFORM)
-endif
-
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
-    device/qcom/sepolicy-legacy-um/public
+    device/qcom/sepolicy/generic/public
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/qcom/sepolicy-legacy-um/private
+    device/qcom/sepolicy/generic/private
+
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
+    device/qcom/sepolicy/qva/public
+
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    device/qcom/sepolicy/qva/private
+
+ifneq (,$(filter $(UM_4_14_FAMILY), $(TARGET_BOARD_PLATFORM)))
+    BOARD_SEPOLICY_DIRS += \
+        device/qcom/sepolicy/generic/vendor/common \
+        device/qcom/sepolicy/qva/vendor/common/sysmonapp \
+        device/qcom/sepolicy/qva/vendor/ssg \
+        device/qcom/sepolicy/generic/vendor/timeservice \
+        device/qcom/sepolicy/qva/vendor/common
+
+    ifeq ($(TARGET_SEPOLICY_DIR),)
+      BOARD_SEPOLICY_DIRS += device/qcom/sepolicy/generic/vendor/$(TARGET_BOARD_PLATFORM)
+      BOARD_SEPOLICY_DIRS += device/qcom/sepolicy/qva/vendor/$(TARGET_BOARD_PLATFORM)
+    else
+      BOARD_SEPOLICY_DIRS += device/qcom/sepolicy/generic/vendor/$(TARGET_SEPOLICY_DIR)
+      BOARD_SEPOLICY_DIRS += device/qcom/sepolicy/qva/vendor/$(TARGET_SEPOLICY_DIR)
+    endif
+
+    ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
+      BOARD_SEPOLICY_DIRS += device/qcom/sepolicy/generic/vendor/test
+      BOARD_SEPOLICY_DIRS += device/qcom/sepolicy/qva/vendor/test
+    endif
+else
+    BOARD_SEPOLICY_DIRS += \
+        device/qcom/sepolicy/legacy/vendor/common/sysmonapp \
+        device/qcom/sepolicy/legacy/vendor/ssg \
+        device/qcom/sepolicy/legacy/vendor/timeservice \
+        device/qcom/sepolicy/legacy/vendor/common
+
+    ifeq ($(TARGET_SEPOLICY_DIR),)
+      BOARD_SEPOLICY_DIRS += device/qcom/sepolicy/legacy/vendor/$(TARGET_BOARD_PLATFORM)
+    else
+      BOARD_SEPOLICY_DIRS += device/qcom/sepolicy/legacy/vendor/$(TARGET_SEPOLICY_DIR)
+    endif
+    ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
+      BOARD_SEPOLICY_DIRS += device/qcom/sepolicy/legacy/vendor/test
+    endif
+endif
 
 -include device/lineage/sepolicy/qcom/sepolicy.mk
